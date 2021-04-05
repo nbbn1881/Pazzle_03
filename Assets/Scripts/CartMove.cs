7deb7db821270dd2ec768bbdc5f919faeca848d7
nbbn1881 <nbbn1881@yahoo.co.jp>
2021-04-06 00:16:41 +0900

    Test03
    Test03

--- Assets/Scripts/CartMove.cs
+++ Assets/Scripts/CartMove.cs
@@ -6,31 +6,60 @@ using DG.Tweening;
 
 public class CartMove : MonoBehaviour
 {
-    Vector3 thisObjPosition;
+
+	Vector3 northRay = new Vector3(0,-5.0f,5.0f);	//北
+	Vector3 SouthRay = new Vector3(0,-5.0f,-5.0f);	//南
+	Vector3 EastRay =  new Vector3(5,-5.0f,0);		//東
+	Vector3 WestRay =  new Vector3(-5,-5.0f,0);		//西
+
+	RaycastHit hit;
+
+	[SerializeField]
+	bool isEnable = false;
+
+	void OnDrawGizmos()
+	{
+		if (isEnable == false)
+			return;
+
+		var scale = transform.lossyScale.x * 0.5f;
+
+		var isHit = Physics.Raycast (transform.position, northRay, out hit, 10);
+		if (isHit) {
+			//Gizmos.DrawRay (transform.position, transform.forward * hit.distance);
+			Gizmos.color = Color.red;
+			Gizmos.DrawRay (transform.position, northRay * hit.distance);
+		} else {
+			Gizmos.DrawRay (transform.position, transform.forward * 10);
+		}
+	}
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        //StartCoroutine("Corou1");
-        DOVirtual.DelayedCall (3f, ()=>Setup());
+        DOVirtual.DelayedCall (3f, ()=>Setup());//3秒遅延
     }
 
-    // Update is called once per frame
-    void Update()
-    {
 
-    }
 
     void Setup()
     {
-        this.gameObject.transform.DOLocalMove(new Vector3(0, 0, 0.9f), 5f).SetRelative();
+        this.gameObject.transform.DOLocalMove(new Vector3(0, 0, 0.9f), 5f)//5秒かけて移動
+            .SetRelative()          //相対移動
+            .SetEase(Ease.Linear)  //一定の速さ
+			.OnComplete(() =>
+			{
+				NextMove();
+			});
     }
 
-    /*
-    private IEnumerator Corou1()//コルーチン1
+    void NextMove()
     {
-        Debug.Log("コルーチン");
-        yield return new WaitForSeconds(2.0f);
-        Debug.Log("2秒待った");
-        Setup();
-    }*/
+
+    }
+
+
 }
